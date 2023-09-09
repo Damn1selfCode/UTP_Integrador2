81978f57354b505999f050abfaffe164d7e2980e
Damn1selfCode <112675720+Damn1selfCode@users.noreply.github.com>
2023-09-08 22:41:40 -0500

    1er SPRINT

---
+++ AuthModule/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Panel de Streaming</h1>
+@stop
+
+@section('content')
+    <p>Vienvenido a la página backoffice.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
